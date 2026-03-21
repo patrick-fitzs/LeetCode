e9e89b433cb73392150b96facdf3a158e78ec922
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2026-03-21 19:28:10 +0000

    update 69

--- Problem List/Easy/69. Sqrt(x).py
+++ Problem List/Easy/69. Sqrt(x).py
@@ -1,17 +1,36 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        # Binary search
-        left, right = 1, x
-        while left <= right:
-            mid = (left + right) // 2
 
-            if mid * mid == x:
-                return mid
-            elif mid * mid < x:
-                left = mid + 1
-            else:
-                right = mid - 1
+"""
+https://leetcode.com/problems/sqrtx/
 
-        return right
+so to find the square root of 9 for eg, the answer is betwene 0 and 9
+so we search that space, multiplying x by x, instead of brute force 0 to 9, we
+can use binary search to change from O(n) to O(log n)
+
+e.g. 9+1=10
+            left=1, right = 9//2 = 4
+            mid = (1+4)//5 = 2
+            2*2=4, is this 9? no
+
+            left = mid+1 so 2+1=3
+            right = 4
+            mid = (3+4)//2 = 7 // 2 = 3
+            3*3=9, is this 9? YES
+
+"""
+def mySqrt(x):
+    if x < 2:
+        return x
+
+    #initiate binary search from here
+    left, right = 1, x //2
+    while left <= right:
+        mid = (left + right) // 2
+
+        if mid * mid == x:
+            return mid
+        elif mid * mid < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return right
