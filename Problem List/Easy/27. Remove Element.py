8ec29c42b2b6386a3d8262dbeb0ce06df2c0b039
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2026-03-22 13:12:25 +0000

    27. Remove Element

--- Problem List/Easy/27. Remove Element.py
+++ Problem List/Easy/27. Remove Element.py
@@ -1,52 +1,23 @@
 """
-Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
-The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+https://leetcode.com/problems/remove-element/
 
-Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+So we need to count occurences of k in a list called nums
 
-Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
-The remaining elements of nums are not important as well as the size of nums.
-Return k.
-
-
-Custom Judge:
-The judge will test your solution with the following code:
-
-int[] nums = [...]; // Input array
-int val = ...; // Value to remove
-int[] expectedNums = [...]; // The expected answer with correct length.
-                            // It is sorted with no values equaling val.
-
-int k = removeElement(nums, val); // Calls your implementation
-
-assert k == expectedNums.length;
-sort(nums, 0, k); // Sort the first k elements of nums
-for (int i = 0; i < actualLength; i++) {
-    assert nums[i] == expectedNums[i];
-}
-If all assertions pass, then your solution will be accepted.
-
-
-
-Example 1:
-
-Input: nums = [3,2,2,3], val = 3
-Output: 2, nums = [2,2,_,_]
-Explanation: Your function should return k = 2, with the first two elements of nums being 2.
-It does not matter what you leave beyond the returned k (hence they are underscores).
+so we iterate, does i == val? if not skip
+               does i+1 == value? yes so counter++
+               also replace current i with number that is not val, so the index the counter holds
 """
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
         # create index counter
-        index = 0
-        # iterate over list
+        k = 0
+
         for i in range(len(nums)):
-            # if current point in list is not = to val
+            # does 3!= val? yes so nums[k(0)]
             if nums[i] != val:
-                # first index num is now nums[i]... then second and so on
-                nums[index] = nums[i]
-                # increment for next loop
-                index += 1
-        return index
+                nums[k] = nums[i]
+                k += 1
+
+        return k
 
 print(Solution().removeElement([3,2,2,3], 3))
