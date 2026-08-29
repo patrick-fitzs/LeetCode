c60c80c67c768496306bfc096a613503c729bf06
patrickfitzsimons <patrickfitzs17@hotmail.com>
2026-08-29 20:47:34 +0100

    add comments

--- Problem List/Easy/349. Intersection of Two Arrays.py
+++ Problem List/Easy/349. Intersection of Two Arrays.py
@@ -1,6 +1,13 @@
 from typing import List
 
 
+'''
+
+Return the intersection of 2 arrays. This just means the common nums between them
+Literally the intersection of a set
+
+'''
+
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         s1 = set(nums1) # get rid of dupes
