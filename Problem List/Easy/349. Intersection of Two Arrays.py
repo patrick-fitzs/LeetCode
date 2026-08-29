015455c76a778011778f8a23fd276b8c4daaf87f
patrickfitzsimons <patrickfitzs17@hotmail.com>
2026-08-29 20:46:28 +0100

    added 349 with hash lookup solution

---
+++ Problem List/Easy/349. Intersection of Two Arrays.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        s1 = set(nums1) # get rid of dupes
+
+        result = [] # store results here to return
+
+        for num in nums2:
+            if num in s1:
+                result.append(num)
+                s1.remove(num)
+
+        return result
+
+
+
+print(Solution().intersection(nums1=[1,2,2,1], nums2=[2,2]))
