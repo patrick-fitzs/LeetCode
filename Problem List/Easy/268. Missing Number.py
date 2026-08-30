b14a90e8db6fe58425d3e75ca159294e31b71366
patrickfitzsimons <patrickfitzs17@hotmail.com>
2026-08-30 20:10:38 +0100

    update

--- Problem List/Easy/268. Missing Number.py
+++ Problem List/Easy/268. Missing Number.py
@@ -4,10 +4,12 @@ from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         num_set = set(nums)
+        #print(num_set) # set for O(1) look up
 
 
-        for i in range(len(nums) +1):
-            if i not in num_set:
+
+        for i in range(len(nums) +1): # loop from  0 - n
+            if i not in num_set:  # if not in our set, return it
                 return i
 
 
@@ -15,5 +17,5 @@ class Solution:
 
 
 print(Solution().missingNumber([3, 0, 1]))
-print(Solution().missingNumber([0, 1]))
-print(Solution().missingNumber([9,6,4,2,3,5,7,0,1]))
+#print(Solution().missingNumber([0, 1]))
+#print(Solution().missingNumber([9,6,4,2,3,5,7,0,1]))
