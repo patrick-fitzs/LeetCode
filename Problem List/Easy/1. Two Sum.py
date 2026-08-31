8a42ec21db2bf1760e326e721e71e3370080a65c
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2026-08-31 10:49:25 +0100

    update solution

--- Problem List/Easy/1. Two Sum.py
+++ Problem List/Easy/1. Two Sum.py
@@ -15,16 +15,27 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """
+from typing import List
+
 
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        map = {}
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
         for i, num in enumerate(nums):
-            print(i, num)
-            complement_num = target - num
-            if complement_num in map:
-                return [map[complement_num], i]
-            map[num] = i
+            comp_num = target - num
 
+            if comp_num in seen:
+                return seen[comp_num], i
+            seen[num] = i
+''' 
+ This is what iterations look like
+    0, 1, 2, 3
+    2, 7, 11, 15 - this is the enum
 
+    target = 9
+    compnum = 2  (9-7, the 2nd iteration)
+    {2: 0, } - added from the first iteration seen{} num which is 2 so seen{2:} = 0 so seen{2:0}
+    return {0, 1}
+'''
 print(Solution().twoSum([2, 7, 11, 15], 9))
