721d2fe5f358bec73f3474876fcad65a2a65ed92
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2026-09-06 19:32:42 +0100

    update 347

--- Problem List/Medium/347. Top K Frequent Elements.py
+++ Problem List/Medium/347. Top K Frequent Elements.py
@@ -1,27 +1,36 @@
-from collections import defaultdict, Counter
 from typing import List
+from collections import defaultdict, Counter
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
+        if len(nums) == k:
+            return nums # gives us O(1) / straight return
 
-        charCounter = {} # out dict to store count of elems
+        numCounter = {} # our dict to hold counts
+
         for num in nums:
-            if num not in charCounter:
-                charCounter[num] = 1 # start with 1 if not already there
+            if num not in numCounter:
+                numCounter[num] = 1 # create the num count if not in our dict
 
             else:
-                charCounter[num] += 1 # if already there add 1
+                numCounter[num] += 1 # increment by 1 if it is
 
 
-        mostFreq = Counter(charCounter).most_common(k) # wrap in a counter, call most_common k elems
+        mostFrequent = Counter(numCounter).most_common(k) # a Counter function to grab most freq elements
 
-        keys = [item[0] for item in mostFreq] # iterate and grab just the keys
-
-
-        return keys
+        key = [item[0] for item in mostFrequent] # iterate and grab keys
 
 
+        return key
 
-print(Solution().topKFrequent(nums=[1,1,1,2,2,3], k=2))
+
+
+
+
+
+
+
+
+print(Solution().topKFrequent(nums=[1,1,1,2,2,3], k = 2))
