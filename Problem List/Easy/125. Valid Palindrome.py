44200ea7faf97f732a5983dff287b8b6ae09884a
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2026-09-14 20:47:34 +0100

    add

--- Problem List/Easy/125. Valid Palindrome.py
+++ Problem List/Easy/125. Valid Palindrome.py
@@ -2,26 +2,29 @@ import re
 
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^a-zA-Z0-9]', "", s).lower()
-        return s == s[::-1]
-
-
+#     def isPalindrome(self, s: str) -> bool:
+#         s = re.sub('[^a-zA-Z0-9]', "", s).lower()
+#         return s == s[::-1]
 #
-# Straight forward approach using re library and a method sub which removes the following characters.
-# lastly we use.lower and return s vs s in reverse for a true or false
-print(Solution().isPalindrome("abba"))
-print(Solution().isPalindrome("Defff"))
+#
+# #
+# # Straight forward approach using re library and a method sub which removes the following characters.
+# # lastly we use.lower and return s vs s in reverse for a true or false
+
 
 # below is a slider method #
-# def isPalindrome(self, s: str) -> bool:
-#     s = ''.join(c.lower() for c in s if c.isalnum())
-#         left = 0
-#         right = len(s) - 1
-#
-#         while left < right:
-#             if s[left] != s[right]:
-#                 return False
-#             left += 1
-#             right -=1
-#         return True
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -=1
+        return True
+
+
+print(Solution().isPalindrome("abba"))
+print(Solution().isPalindrome("Defff"))
